020fadd5cd5879e440bd8d8ebbf9fe365e52d48e
Aedan <aedan.giffin@gmail.com>
2018-11-26 03:19:24 +0000

    adding ingredients

--- ingredients.js
+++ ingredients.js
@@ -1,1 +1,15 @@
 var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var counter = 0;
+
+while (counter < ingredients.length){
+console.log(ingredients[counter]);
+counter++;
+}
+
+for(counter = 0; counter < ingredients.length; ++counter){
+  console.log(ingredients[counter]);
+}
+
+for (counter = ingredients.length - 1; counter > 0; counter--){
+  console.log(ingredients[counter]);
+}
